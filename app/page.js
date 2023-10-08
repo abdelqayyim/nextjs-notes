482ca8f6d431aeb48176177f0a3b2b36746e8bba
abdelqayyim <abdelymaster@gmail.com>
2023-10-07 23:08:45 -0400

    Restarting

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    
+    <div><Link href="/pages">Helllo</Link></div>
+  )
+}
