2eff1fbf977658085dcb35a8d7d3756277866868
abdelqayyim <abdelymaster@gmail.com>
2023-10-29 14:22:32 -0400

    Fixed case sensitivity

---
+++ app/components/Button/LanguageButton.js
@@ -0,0 +1,29 @@
+import React, { useCallback, useContext } from "react";
+import styles from "./LanguageButton.module.css";
+// import { ACTIONS, AppProvider } from "../../app/AppContext";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+const LanguageButton = (props) => {
+  // const curr = useContext(AppProvider); // global context
+  const router = useRouter();
+
+  
+  //TODO: remove the spaces before passing it to the parameters or else you'll have %
+
+  return (
+    <Link href={`/${props.name.replace(/\s/g, "")}`} as={ `${props.name.replace(/\s/g, "")}`} className={styles.link}>
+      <button
+      className={styles.btn}
+      onClick={props.clicked}
+      name={props.name}
+      id={props._id}
+    >
+      {props.name}
+    </button>
+    </Link>
+    
+  );
+};
+
+export default LanguageButton;
---
+++ app/components/Button/NoteLanguage.js
@@ -0,0 +1,27 @@
+import React, {Fragment, useState} from 'react'; 
+// import ChangeNoteLang from '../PopUps/ChangeNoteLang';
+import styles from './NoteLanguage.module.css';
+
+const NoteLanguage = (props) => {
+    const [showNoteChange, setShowNoteChange] = useState(false);
+    const changeLang = (lang) => {
+        setShowNoteChange(true);
+    }
+    const closePop = () => {
+        setShowNoteChange(false);
+    }
+    const submitChange = (newLang) => {
+        props.changeLanguage(newLang)
+        setShowNoteChange(false);
+    }
+    return (
+        <Fragment>
+            <div className={ styles["note-lang"]} onClick={changeLang} >
+                {props.noteLanguage === "cs" ? "c#" : props.noteLanguage}
+            </div>
+            {/* {showNoteChange && <ChangeNoteLang closePop={closePop} saveChange={ submitChange} /> } */}
+        </Fragment>
+    )
+};
+
+export default NoteLanguage;
